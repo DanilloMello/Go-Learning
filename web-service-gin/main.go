a66341898638a49702abebf8b917881cec922a91
danillo <danillomello.developer@gmail.com>
2023-09-02 16:21:23 -0300

    crud gin

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -29,6 +29,7 @@ func getAlbums(c *gin.Context) {
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
 	router.Run("localhost:8080")
 }
